49b6c6ca354de1b397a8e0bf893369b520df2553
Lau <laurafernandat5@gmail.com>
2025-11-17 18:32:16 -0400

    Cambio tittle

--- LMH/config/constants.php
+++ LMH/config/constants.php
@@ -1,5 +1,5 @@
 <?php
-define('SITE_NAME', 'LuxMaxHeu');
+define('SITE_NAME', 'LMH Innovations & Solutions');
 define('EMAIL', 'LMH.innovations.and.solutions@gmail.com
 ');
 define('PHONE', '+58 412-829-3284');
